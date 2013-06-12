c3595f974d5ace5e93e8c83dbb6dad2737548ea9
Siddique Hameed <siddii+git@gmail.com>
2013-06-12 11:45:49 -0500

    fixing tab selection

--- background.js
+++ background.js
@@ -2,17 +2,13 @@ chrome.browserAction.onClicked.addListener(gotoMainpage);
 
 function gotoMainpage() {
     console.log('Going to inbox...');
-    chrome.tabs.getAllInWindow(undefined, function(tabs) {
+    chrome.tabs.getAllInWindow(undefined, function (tabs) {
         for (var i = 0, tab; tab = tabs[i]; i++) {
             if (tab.url && tab.url === getMainUrl()) {
-                console.log('Found Gmail tab: ' + tab.url + '. ' +
-                    'Focusing and refreshing count...');
                 chrome.tabs.update(tab.id, {selected: true});
-                //startRequest({scheduleRequest:false, showLoadingAnimation:false});
                 return;
             }
         }
-        console.log('Could not find Gmail tab. Creating one...');
         chrome.tabs.create({url: getMainUrl()});
     });
 }
