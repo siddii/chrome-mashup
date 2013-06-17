c5c674c303957fd4af8f69a0a76542c8d2e05750
Siddique Hameed <siddii+git@gmail.com>
2013-06-17 09:08:38 -0500

    WIP

--- js/mashup.js
+++ js/mashup.js
@@ -1,5 +1,31 @@
 
+
 angular.module('mashup', [], function (){
     console.log('Mashup module initialised');
-    $('[data-toggle="popover"]').popover();
+
+    var iframe = document.getElementById('theFrame');
+    var message = {
+        command: 'render',
+        context: {thing: 'world'}
+    };
+    iframe.contentWindow.postMessage(message, '*');
+
+
+    window.addEventListener('message', function(event) {
+        if (event.data.html) {
+            console.log('Returned = ', event.data.html);
+//            var notification = webkitNotifications.createNotification(
+//                'icon.png',
+//                'Templated!',
+//                'HTML Received for "' + event.data.name + '": `' + event.data.html + '`'
+//            );
+//            notification.show();
+        }
+    });
+
+//
+//    $.get("https://www.google.com").success(
+//        function(data) {
+//            console.log('data = ', data);
+//        });
 });
